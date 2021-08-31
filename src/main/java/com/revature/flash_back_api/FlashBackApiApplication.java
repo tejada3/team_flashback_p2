148664410d411a501e09d53292c47f8284f173b6
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-08-31 19:35:05 -0400

    Plain ol project

---
+++ src/main/java/com/revature/flash_back_api/FlashBackApiApplication.java
@@ -0,0 +1,13 @@
+package com.revature.flash_back_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashBackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashBackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/revature/flash_back_api/FlashBackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.flash_back_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashBackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
