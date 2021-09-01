04cd467eff54eae1d6293864d656b943281cdf08
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-08-31 22:04:33 -0400

    Added data models

--- src/main/java/com/revature/flash_back_api/FlashBackApiApplication.java
+++ src/main/java/com/revature/flash_back_api/FlashBackApiApplication.java
@@ -1,12 +1,29 @@
 package com.revature.flash_back_api;
 
+import com.revature.flash_back_api.models.TriviaCard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class FlashBackApiApplication {
 
     public static void main(String[] args) {
+        TriviaCard triviaCard = new TriviaCard();
+        List<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        System.out.println(data.toString());
+        System.out.println(data.get(0));
+        data.remove("A");
+        data.add(1, "H");
+
+        System.out.println(data.get(1));
+        System.out.println(data.toString());
+
         SpringApplication.run(FlashBackApiApplication.class, args);
     }
 
---
+++ src/main/java/com/revature/flash_back_api/models/Thread.java
@@ -0,0 +1,72 @@
+package com.revature.flash_back_api.models;
+
+import java.util.Objects;
+
+public class Thread {
+    private String threadId;
+    private String userID;
+    private String threadTitle;
+    private String threadContent;
+
+    public Thread(String threadId, String userID) {
+        this.threadId = threadId;
+        this.userID = userID;
+    }
+
+    public String getThreadId() {
+        return threadId;
+    }
+
+    public void setThreadId(String threadId) {
+        this.threadId = threadId;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getThreadTitle() {
+        return threadTitle;
+    }
+
+    public void setThreadTitle(String threadTitle) {
+        this.threadTitle = threadTitle;
+    }
+
+    public String getThreadContent() {
+        return threadContent;
+    }
+
+    public void setThreadContent(String threadContent) {
+        this.threadContent = threadContent;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Thread)) return false;
+        Thread thread = (Thread) o;
+        return getThreadId().equals(thread.getThreadId()) && getUserID().equals(thread.getUserID()) && getThreadTitle().equals(thread.getThreadTitle()) && getThreadContent().equals(thread.getThreadContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getThreadId(), getUserID(), getThreadTitle(), getThreadContent());
+    }
+
+
+    @Override
+    public String toString() {
+        return "Thread{" +
+                "threadId='" + threadId + '\'' +
+                ", userID='" + userID + '\'' +
+                ", threadTitle='" + threadTitle + '\'' +
+                ", threadContent='" + threadContent + '\'' +
+                '}';
+    }
+}
