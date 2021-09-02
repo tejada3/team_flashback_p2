370a23a974f2a22039b09f558f1ce11fd5f83df8
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-02 10:27:44 -0500

    Made a Principal DTO.

--- src/main/java/com/revature/flash_back_api/web/dtos/Principal.java
+++ src/main/java/com/revature/flash_back_api/web/dtos/Principal.java
@@ -1,4 +1,64 @@
 package com.revature.flash_back_api.web.dtos;
 
+
+import java.util.Objects;
+
+// This is a regular POJO and does not need to be initialized via Spring
 public class Principal {
+    private String id;
+    private String username;
+    private String role;
+
+    public Principal() { super(); }
+
+    // TODO: Make a UsersDTO Constructor here!
+    public Principal(UsersDTO subject) {
+        this.id = subject
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Principal principal = (Principal) o;
+        return Objects.equals(id, principal.id) && Objects.equals(username, principal.username) && Objects.equals(role, principal.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Principal{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
