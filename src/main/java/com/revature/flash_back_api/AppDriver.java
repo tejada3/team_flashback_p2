dd00402440f40837a00bf7604677abf5e73b80cb
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-02 14:10:56 -0500

    Implemented basic Spring Boot with Mongo support.

--- src/main/java/com/revature/flash_back_api/AppDriver.java
+++ src/main/java/com/revature/flash_back_api/AppDriver.java
@@ -1,2 +1,11 @@
-package com.revature.flash_back_api;public class AppDriver {
+package com.revature.flash_back_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Implies ComponentScan, and some other stuff.
+public class AppDriver {
+    public static void main(String[] args) {
+        SpringApplication.run(AppDriver.class, args);
+    }
 }
--- src/main/java/com/revature/flash_back_api/config/AppConfig.java
+++ src/main/java/com/revature/flash_back_api/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 // This class is used for programmatic bean registration with the container (aka: ApplicationContext)
 @Configuration
 @ComponentScan("com.revature") // if no package string is provided, then Spring will scan the package that this class is in for components
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 public class AppConfig {
 
 
