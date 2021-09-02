6fad22cf8109391cbe55ed9ad6bd94fd2d02a481
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-02 10:44:09 -0500

    Made Flashback API CORS Compliant.

---
+++ src/main/java/com/revature/flash_back_api/web/util/CorsFilter.java
@@ -0,0 +1,24 @@
+package com.revature.flash_back_api.web.util;
+
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setAttribute("filtered", true);
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Expose-Headers", "Authorization");
+        chain.doFilter(req, res);
+    }
+}
