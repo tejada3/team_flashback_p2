cd2d5930366e19a013b3e0939e0b326722d55b52
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-09-10 09:28:02 -0400

    trivia card updates

--- src/main/java/com/revature/flash_back_api/web/controllers/TriviaCardSetController.java
+++ src/main/java/com/revature/flash_back_api/web/controllers/TriviaCardSetController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trivia/set")
+@RequestMapping("/trivia/set")git
 public class TriviaCardSetController {
 
     private TriviaCardSetsService triviaCardSetsService;
