5a9cc42df82ba211fa08e62b401492fed12f43a8
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-11 15:13:44 -0500

    Added an endpoint to query with the Subforum ID in order to grab all threads associated with that forum.

--- src/main/java/com/revature/flash_back_api/services/ForumService.java
+++ src/main/java/com/revature/flash_back_api/services/ForumService.java
@@ -1,7 +1,9 @@
 package com.revature.flash_back_api.services;
 
 import com.revature.flash_back_api.models.repos.SubforumRepository;
+import com.revature.flash_back_api.models.repos.ThreadRepository;
 import com.revature.flash_back_api.web.dtos.SubforumDTO;
+import com.revature.flash_back_api.web.dtos.ThreadDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,14 +13,22 @@ import java.util.stream.Collectors;
 public class ForumService {
 
     private final SubforumRepository subforumRepo;
+    private final ThreadRepository threadRepo;
 
     @Autowired
-    ForumService(SubforumRepository subforumRepo) { this.subforumRepo = subforumRepo; }
+    ForumService(SubforumRepository subforumRepo, ThreadRepository threadRepo) { this.subforumRepo = subforumRepo; this.threadRepo = threadRepo; }
 
     public List<SubforumDTO> findAllSubforums() {
         return subforumRepo.findAll()
                 .stream()
                 .map(SubforumDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<ThreadDTO> findAllThreads(String subforumId) {
+        return threadRepo.findThreadsBySubforumId(subforumId)
+                .stream()
+                .map(ThreadDTO::new)
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/com/revature/flash_back_api/web/controllers/ForumController.java
+++ src/main/java/com/revature/flash_back_api/web/controllers/ForumController.java
@@ -2,10 +2,9 @@ package com.revature.flash_back_api.web.controllers;
 
 import com.revature.flash_back_api.services.ForumService;
 import com.revature.flash_back_api.web.dtos.SubforumDTO;
+import com.revature.flash_back_api.web.dtos.ThreadDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +21,7 @@ public class ForumController {
 
     @GetMapping(path = "/get-subforum", produces = "application/json")
     public List<SubforumDTO> getAllSubforums() { return forumService.findAllSubforums(); }
+
+    @PostMapping(path="/get-threads", produces = "application/json", consumes = "application/json")
+    public List<ThreadDTO> getThreadsBySubforumId(@RequestBody String subforumId) { return forumService.findAllThreads(subforumId); }
 }
