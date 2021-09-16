fb906e96fa7e65a45da07e9968e12de49ff21ae2
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-16 10:17:48 -0500

    Started writing testing.

--- src/test/com/revature/flash_back_api/services/TriviaCardServiceTest.java
+++ src/test/com/revature/flash_back_api/services/TriviaCardServiceTest.java
@@ -1,19 +1,27 @@
 package com.revature.flash_back_api.services;
 
+import com.revature.flash_back_api.models.repos.TriviaCardRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class TriviaCardServiceTest {
 
+    TriviaCardService sut;
+    private TriviaCardRepository mockTriviaCardRepo;
+
     @BeforeEach
     void setUp() {
+        mockTriviaCardRepo = mock(TriviaCardRepository.class);
+        sut = new TriviaCardService(mockTriviaCardRepo);
     }
 
     @AfterEach
     void tearDown() {
+        sut = null;
     }
 
     @Test
--- src/test/com/revature/flash_back_api/services/TriviaCardSetsServiceTest.java
+++ src/test/com/revature/flash_back_api/services/TriviaCardSetsServiceTest.java
@@ -1,19 +1,27 @@
 package com.revature.flash_back_api.services;
 
+import com.revature.flash_back_api.models.repos.TriviaCardSetsRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class TriviaCardSetsServiceTest {
 
+    TriviaCardSetsService sut;
+    private TriviaCardSetsRepository mockTCSRepo;
+
     @BeforeEach
     void setUp() {
+        mockTCSRepo = mock(TriviaCardSetsRepository.class);
+        sut = new TriviaCardSetsService(mockTCSRepo);
     }
 
     @AfterEach
     void tearDown() {
+        sut = null;
     }
 
     @Test
